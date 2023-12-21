c00c04d8a53262c235358047791f3f7b07c04e0b
Miroslav Kováč <assembler_bot@yahoo.com>
2023-12-21 14:24:06 +0100

    [Update] Migration plugin 1.4.0

--- src/Studio/Plugins/Migration/Serializer/Binders/DataSerializationBinder.cs
+++ src/Studio/Plugins/Migration/Serializer/Binders/DataSerializationBinder.cs
@@ -5,6 +5,7 @@ using System.Reflection;
 namespace Migration
 {
 	// Maps data classes to latest versions of migratable classes and to lists of them.
+	// Used in transfer between application data and migration data.
 	
 	// For example, class without custom id:
 	//   MyClass       <-> MyClass_003
@@ -56,6 +57,15 @@ namespace Migration
 				}
 			}
 			
+			// non migratable classes
+			{
+				IEnumerable<Type> non_migratable_classes = MigrationManager.GetTypesWithAttribute(assembly, typeof(NonMigratableIdAttribute));
+				foreach (Type non_migratable_class in non_migratable_classes)
+				{
+					mapping.Add(non_migratable_class, non_migratable_class);
+				}
+			}
+
 			// no obsolete classes
 
 			Init(mapping);
