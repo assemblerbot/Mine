144a1f63d7ffae00c1f35d380b4537164b278896
Miroslav Kovac <assembler_bot@yahoo.com>
2024-02-05 23:16:11 +0100

    [Refactor] Cleanup and notes.

--- src/Studio/Models/References/StudioReference.cs
+++ src/Studio/Models/References/StudioReference.cs
@@ -1,27 +1,20 @@
 using Migration;
 using RedHerring.Studio.Models;
 using RedHerring.Studio.Models.Project.FileSystem;
-using RedHerring.Studio.UserInterface;
 
 namespace Mine.Studio;
 
 [Serializable, SerializedClassId("f82d86eb-7957-4c24-8b8d-3a1e04c86ec6")]
 public abstract class StudioReference
 {
-	public virtual Type   InspectorControlType => typeof(InspectorReferenceControl);
-	public abstract string Name { get; }
+	public abstract string Name { get; } // <-- problematic method
 
 	public string? Guid = null;
 	public bool    IsEmpty => Guid == null;
 
-	public abstract bool            CanAcceptNode(StudioModel studioModel, ProjectNode node);
+	public abstract bool            CanAcceptNode(StudioModel studioModel, ProjectNode node); // <-- problematic method
 	public abstract StudioReference CreateCopy();
 	
-	public virtual void WriteJsonValue(StringWriter stringWriter)
-	{
-		//TODO
-	}
-
 	protected bool Equals(StudioReference other)
 	{
 		return Guid == other.Guid;
