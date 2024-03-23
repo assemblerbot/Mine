eb2a6e1d51910e2b73ddb5f91080411dc719506b
Miroslav Kovac <assembler_bot@yahoo.com>
2024-03-23 16:24:14 +0100

    [Refactor] Added comments to component event functions.

--- src/Framework/World/Component.cs
+++ src/Framework/World/Component.cs
@@ -10,19 +10,29 @@ public class Component
 		_entity = entity;
 	}
 
-	// 'event' functions
+	//--------------------------------------------
+	// 'event' functions:
+	//--------------------------------------------
+
+	// called after ENTITY that owns this component was added to world
+	//  (if the component was added to the entity already in the world, this function is NOT called)
 	public virtual void AfterAddedToWorld()
 	{
 	}
 	
+	// called before ENTITY that owns this component is removed from world (disconnected or destroyed)
 	public virtual void BeforeRemovedFromWorld()
 	{
 	}
 
+	// called before ENTITY is disposed
+	//  (all entities connected to the world are disposed in the moment when world is disposed, disconnected entities needs to be disposed by user)
 	public virtual void Dispose()
 	{
 	}
 
+	// called when ENTITY flags were changed
+	//  (it's guaranteed that oldFlags != newFlags)
 	public virtual void AfterEntityFlagsChanged(EntityFlags oldFlags, EntityFlags newFlags)
 	{
 	}
